7c899cf477a8406156881603562894561adb3667
AnkaBigudi <ankabigudi@ya.ru>
2023-01-29 14:55:16 +0300

    Деление

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,4 +1,4 @@
-﻿int NumberA = 3;
+﻿int NumberA = 3; //int - целые числа
 int NumberB = 5;
 int result = NumberA + NumberB; //можно ввести доп переменную result
 Console.WriteLine(result); //либо (NumberA + NumberB)
--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,8 +1,4 @@
-﻿Console.Clear();
-Console.Write("Введите число: ");
-string number1 = Console.WriteLine("понедельник");
-string number2 = Console.WriteLine("понеик");
-string number3 = Console.WriteLine("понеде");
-string number4 = Console.WriteLine("едельник");
-string number5 = Console.WriteLine("пльник");
-Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+﻿double NumberA = 18; //double деление с остатком чтоб показывал
+double NumberB = 5;
+double result = NumberA/NumberB;
+Console.WriteLine(result);
