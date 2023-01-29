cf2581141746a70f2a388d6f1a8b7ca3019b87b3
AnkaBigudi <ankabigudi@ya.ru>
2023-01-29 15:13:25 +0300

    Рандомные числа

---
+++ Ex005/Program.cs
@@ -0,0 +1,5 @@
+﻿int NumberA = new Random().Next(1, 99); //new Random().Next(min, max) где max=max-1
+Console.WriteLine(NumberA); //чтобы видеть какое рандомное число выбрано
+int NumberB = new Random().Next(1, 99); //диапазон будет от 1 до 98
+Console.WriteLine(NumberB);
+Console.Write(NumberA + NumberB);
