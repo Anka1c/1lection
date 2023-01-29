c504e885251f88812c8653f840e9c55d95ab873b
AnkaBigudi <ankabigudi@ya.ru>
2023-01-29 16:09:53 +0300

    If-Else

---
+++ Ex006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name ");
+String Name = Console.ReadLine();
+if(Name.ToLower() == "anna")     //true       .ToLower() - считывает все буквы (большие, маленькие; anna, aNNa...)
+{
+  Console.WriteLine("Good Morning, my Queen Anna");
+}
+else                             //false
+{
+  Console.Write("Hi King ");
+  Console.WriteLine(Name);
+}
