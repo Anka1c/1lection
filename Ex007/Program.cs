93fff3404cce79d97f4a8902f6aec7e2fccbb703
AnkaBigudi <ankabigudi@ya.ru>
2023-01-29 16:35:12 +0300

    Макс вес гири

---
+++ Ex007/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 11;
+int b = 8;
+int c = 10;
+int d = 9;
+int e = 16;
+
+int max = a; //max наша коробочка и мы кладем в нее значение первой гири (a)
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Максимальный вес гири равен ");
+Console.Write(max);
